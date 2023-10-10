2468f898ed785b3410ab6b50c982ebf87d7757de
Belinda Dunu <139175163+belindadunu@users.noreply.github.com>
2023-10-10 19:45:33 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,29 @@
+# based on https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
+
+provider "aws" {
+  access_key = "" # input access_key here
+  secret_key = "" # input secret_key here
+  region = "us-east-1"
+  #profile = "Admin"
+}
+
+# create instance
+
+resource "aws_instance" "web_server2" {
+  ami           = "ami-x" # input AMI here
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  subnet_id     = "" # input subnet ID here
+  security_groups = [""] # input security group id here
+
+  user_data = "${file("jenkins_installation.sh")}"
+
+    tags = {
+    "Name": "tf_made_instance"
+  }
+}
+
+# output new instance IP
+output "instance_ip" {
+  value = aws_instance.web_server2.public_ip
+}
