56b9294202de10da156a8c922c972d7ad2b39ac7
Belinda Dunu <139175163+belindadunu@users.noreply.github.com>
2023-10-11 07:12:43 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,30 @@
 # based on https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
 
 provider "aws" {
-  access_key = "" # input access_key here
-  secret_key = "" # input secret_key here
+  access_key = var.access_key
+  secret_key = var.secret_key
   region = "us-east-1"
   #profile = "Admin"
 }
 
 # create instance
 
-resource "aws_instance" "web_server2" {
-  ami           = "ami-x" # input AMI here
-  instance_type = "t2.micro"
+resource "aws_instance" "tf_activity" {
+  ami           = var.ami
+  instance_type = var.instance_type
   associate_public_ip_address = true
-  subnet_id     = "" # input subnet ID here
-  security_groups = ["sg-"] # input security group id here
+  subnet_id     = var.subnet_id
+  security_groups = var.security_groups
+  key_name = var.key_name
 
   user_data = "${file("jenkins_installation.sh")}"
 
     tags = {
-    "Name": "tf_made_instance"
+    "Name": var.instance_name
   }
 }
 
 # output new instance IP
 output "instance_ip" {
-  value = aws_instance.web_server2.public_ip
+  value = aws_instance.tf_activity.public_ip
 }
